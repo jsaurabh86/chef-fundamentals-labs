62a24010884c58159c28539d4aa649f50d4f58fa
vagrant <vagrant@layer201.prod.mint.com>
2016-03-09 22:45:15 +0000

    Created a motd template in workstation cookbook and updated the version to 2.1

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -6,14 +6,8 @@ package 'git' do
 	action :install
 end
 
-file '/etc/motd' do
-	content "Property of ...
-
-	IPADDRESS: #{node['ipaddress']}
-	HOSTNAME: #{node['hostname']}
-	MEMORY: #{node['memory']}
-	CPU: #{node['cpu']['0']['mhz']}
-"
+template '/etc/motd' do
+	source 'motd.erb'
 	mode '0644'
 	owner 'root'
 	group 'root'
